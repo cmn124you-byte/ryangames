30edb8b4f011996249dc01ad4aaeb076c80a2a20
cmn124you-byte <cmn124you-byte@users.noreply.github.com>
2026-08-04 08:03:03 +0100

    رفع ملف من لوحة التحرير (٤‏/٨‏/٢٠٢٦)

--- data.js
+++ data.js
@@ -32,7 +32,7 @@ function svgBanner(emoji, title, c1, c2, tag) {
   return "data:image/svg+xml;charset=utf-8," + encodeURIComponent(svg);
 }
 
-var DATA_VERSION = 23;
+var DATA_VERSION = 24;
 
 var DEFAULT_GAMES = [
   {
