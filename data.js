fa6da278bbeb0e721ba6760e7d5d03a95de50e34
cmn124you-byte <cmn124you-byte@users.noreply.github.com>
2026-08-04 07:21:48 +0100

    تحديث من لوحة التحرير المحلية (٤‏/٨‏/٢٠٢٦)

--- data.js
+++ data.js
@@ -32,7 +32,7 @@ function svgBanner(emoji, title, c1, c2, tag) {
   return "data:image/svg+xml;charset=utf-8," + encodeURIComponent(svg);
 }
 
-var DATA_VERSION = 8;
+var DATA_VERSION = 9;
 
 var DEFAULT_GAMES = [];
 
@@ -44,7 +44,6 @@ var DEFAULT_LESSONS = [
 ];
 
 var DEFAULT_UPDATES = [
-  { id: 4, title: "Sweet Kingdom", ar: "مملكة الحلويات", days: "محدث منذ يوم", link: "" },
   { id: 5, title: "Grill Masters", ar: "أساتذة الشواء", days: "محدث منذ 3 أيام", link: "" },
   { id: 6, title: "Sushi Sensei", ar: "سوشي سينسي", days: "محدث منذ 5 أيام", link: "" },
   { id: 7, title: "Pizza Empire", ar: "إمبراطورية البيتزا", days: "محدث منذ 8 أيام", link: "" },
