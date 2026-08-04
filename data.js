ebc639201e6c5a85f8d1e5a5cd7eb4952fe26399
cmn124you-byte <cmn124you-byte@users.noreply.github.com>
2026-08-04 08:54:55 +0100

    تحديث من لوحة التحرير المحلية (٤‏/٨‏/٢٠٢٦)

--- data.js
+++ data.js
@@ -32,7 +32,7 @@ function svgBanner(emoji, title, c1, c2, tag) {
   return "data:image/svg+xml;charset=utf-8," + encodeURIComponent(svg);
 }
 
-var DATA_VERSION = 30;
+var DATA_VERSION = 31;
 
 var DEFAULT_GAMES = [
   {
